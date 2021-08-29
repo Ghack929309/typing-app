546343c928b7278498dd4d71e27ef75a64545a06
Ghack929309 <ghack929309@gmail.com>
2021-08-29 06:05:19 +0300

    typing 2.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const TIME_STARTER = 10;
+	const [text, setText] = useState("");
+	const [timer, setTimer] = useState(TIME_STARTER);
+	const [countdown, setCountdown] = useState(false);
+	const [wpm, setWpm] = useState(0);
+
+	useEffect(() => {
+		if (timer > 0 && countdown) {
+			setTimeout(() => {
+				setTimer((time) => time - 1);
+			}, 1000);
+		} else if (timer === 0) {
+			endGame();
+		}
+	}, [timer, countdown]);
+
+	const handleChange = (e) => {
+		const { value } = e.target;
+		setText(value);
+	};
+
+	const wordCountCalculator = (text) => {
+		const wordCounter = text.trim().split(" ");
+		return wordCounter.filter((word) => word !== "").length;
+	};
+	const startGame = () => {
+		setCountdown(true);
+		setText("");
+		setWpm();
+		setTimer(TIME_STARTER);
+	};
+	const endGame = () => {
+		clearTimeout(timer);
+		const wpmCounter = wordCountCalculator(text);
+		setWpm(wpmCounter);
+		setCountdown(false);
+	};
+	return (
+		<div>
+			<h1>How fast do you type ?</h1>
+			<textarea value={text} onChange={handleChange} disabled={!countdown} />
+			<h4>Time remaining:{timer}</h4>
+			<button onClick={startGame} disabled={countdown}>
+				Start
+			</button>
+			<h1>words per minutes:{wpm}</h1>
+		</div>
+	);
 }
 
 export default App;
